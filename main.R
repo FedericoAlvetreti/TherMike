fe485ab9409a19271b2dbb93a2708f441be01f41
Federico Alvetreti <alvetreti.1846936@studenti.uniroma1.it>
2023-08-25 16:25:01 +0200

    Add files via upload
    ora in utils ho aggiunto una funzione che si chiama spectro che fa lo spectogramma, ora nei plot non si vedono forti caratterizzazioni delle temperature ma magari dopo preprocessing dati possiamo provare se si vede qualcosa

--- main.R
+++ main.R
@@ -4,20 +4,17 @@ source('utils.R')
 
 # Loading and merging data ------------------------------------------------
 
-our_recordings <- get_audio_objects("processed-recs")
-paper_recordings <- get_audio_objects("processed-paper")
+our_recordings <- get_audio_objects("SL-splash-recordings")
+paper_recordings <- get_audio_objects("paper-processed-recordings")
 tot_recordings <- c(our_recordings,paper_recordings)
 
-
-# Kind of a feature extraction --------------------------------------------
-
-tot_smooth_funcs <- get_smooth_list_from_ps(create.bspline.basis, n_basis = 100, tot_recordings)
-
-# Split in test and train -------------------------------------------------
-
-c(train,test) := train_test_split(tot_smooth_funcs,0.1)
-
+smooth_funcs <- get_smooth_list_from_ps(create.bspline.basis, n_basis = 200, our_recordings)
 
 # Predictions -------------------------------------------------------------
 
-preds <- predict_set(train,test)
+c(train,test) := train_test_split(smooth_funcs,0.1)
+
+KNN_preds <- KNN_predict_set(train,test,5)
+All_NN_preds <- All_NN_predict_set(train,test)
+
+All_NN_predict_audio(train,test[[1]])
--- utils.R
+++ utils.R
@@ -86,43 +86,6 @@ get_audio_objects <- function(directory_path){
   return(result)
 }
 
-# Get the smooth approximation of a the power spectrum of an audio object defined a basis
-get_smooth_from_ps <- function(basis_func, n_basis, audio_object){
-  
-  
-  my_basis <- basis_func(range(0,(audio_object$Sampling_rate/2)),nbasis = n_basis)
-  freq <- audio_object$Frequencies
-  power <- audio_object$Power_spectrum
-  appr <- smooth.basis(argvals = freq, y = power ,fdParobj = my_basis)$fd
-  appr <- eval.fd(0:(audio_object$Sampling_rate/2),appr)
-
-  return(list("PS_smooth_func" = appr,
-              "Label" = audio_object$Label))
-}
-
-# Get smooth approximations from a list of audio objects 
-get_smooth_list_from_ps <- function(basis_func, n_basis, list){
-  
-  n_iter <- length(list)
-  
-  # setup progress bar
-  pb <- txtProgressBar(min = 0,      # Minimum value of the progress bar
-                       max = n_iter, # Maximum value of the progress bar
-                       style = 3,    # Progress bar style (also available style = 1 and style = 2)
-                       width = 50,   # Progress bar width. Defaults to getOption("width")
-                       char = "=")   # Character used to create the bar
-  i = 1 
-  
-  result <- list()
-  for(audio_object in list){
-    result <- append(result, list(get_smooth_from_ps(basis_func, n_basis, audio_object)))
-    setTxtProgressBar(pb, i)
-    i = i + 1 
-  }
-  close(pb) 
-  return(result)
-}
-
 # Thresholding algorithm, detects peaks and compue moving mean given a signal
 ThresholdingAlgo <- function(y,lag,threshold,influence) {
   signals <- rep(0,length(y))
@@ -159,35 +122,99 @@ train_test_split <- function(tot_set,test_ratio){
   return(list("Train" = train, "Test"= test))
 }
 
-# Predict a single audio 
-predict_audio <- function(train_set, new_data){
-  tot_weight <- 0
-  prediction <- 0
-  for(train_audio in train_set){
-    weight <- 1 / L2(train_audio$PS_smooth_func,new_data)
-    tot_weight <- tot_weight + weight
-    prediction <- prediction + weight * train_audio$Label
+# Computes the spectogram of a audio object
+spectro = function(data, nfft=1024, window=256, overlap=128, t0=0, 
+                   plot_spec = T, normalize = T, return_data = F){
+  
+  library(signal)
+  library(oce)
+  
+  # extract signal
+  snd = data$Recording
+  
+  # demean to remove DC offset
+  snd = snd-mean(snd)
+  
+  # determine duration
+  dur = length(snd)/data$Sampling_rate
+  
+  # create spectrogram
+  spec = specgram(x = snd,
+                  n = nfft,
+                  Fs = data$Sampling_rate,
+                  window = window,
+                  overlap = overlap
+  )
+  
+  # discard phase info
+  P = abs(spec$S)
+  
+  # normalize
+  if(normalize){
+    P = P/max(P)  
   }
   
-  prediction <- prediction / tot_weight
+  # convert to dB
+  P = 10*log10(P)
   
-  return(prediction)
+  # config time axis
+  if(t0==0){
+    t = as.numeric(spec$t)
+  } else {
+    t = as.POSIXct(spec$t, origin = t0)
+  }
+  
+  # rename freq
+  f = spec$f
+  
+  if(plot_spec){
+    
+    # change plot colour defaults
+    par(bg = "black")
+    par(col.lab="white")
+    par(col.axis="white")
+    par(col.main="white")
+    
+    # plot spectrogram
+    imagep(t,f, t(P), col = oce.colorsViridis, drawPalette = T,
+           ylab = 'Frequency [Hz]', axes = F)
+    
+    box(col = 'white')
+    axis(2, labels = T, col = 'white')
+    title(main = data$Label)
+    # add x axis
+    if(t0==0){
+      
+      axis(1, labels = T, col = 'white')
+      
+    }else{
+      
+      axis.POSIXct(seq.POSIXt(t0, t0+dur, 10), side = 1, format = '%H:%M:%S', col = 'white', las = 1)
+      mtext(paste0(format(t0, '%B %d, %Y')), side = 1, adj = 0, line = 2, col = 'white')
+      
+    }
+  }
+  
+  if(return_data){
+    
+    # prep output
+    spec = list(
+      t = t,
+      f = f,
+      p = t(P)
+    )
+    
+    return(spec)  
+  }
 }
 
-# Predict a set of audios
-predict_set <- function(train_set,test_set){
-  preds <- data.frame()
-  for(audio in test_set){
-    preds <- rbind(preds,
-                   c(audio$Label,predict_audio(train_set, audio$PS_smooth_func)))
-  }
-  names(preds) <- c("Label", "Prediction")
-  preds <- preds[order(preds$Label),]
-}
 
 # Distances ---------------------------------------------------------------
 
 L2 <- function(f1, f2){return(sqrt(sum((f1 - f2)^2)))}
+Kernel <- function(x) {
+  return(0.5*exp(-0.5*x^2))
+}
 
 # Plots -------------------------------------------------------------------
 
@@ -218,5 +245,113 @@ labels_hist <- function(recordings){
   labels <- unlist(labels)
   hist(labels)
   
+}
+
+
+# Functional features extraction and prediction ---------------------------
+
+# Get the smooth approximation of a the power spectrum of an audio object defined a basis
+get_smooth_from_ps <- function(basis_func, n_basis, audio_object){
+  
+  my_basis <- basis_func(range(0,(audio_object$Sampling_rate/2)),nbasis = n_basis)
+  freq <- audio_object$Frequencies
+  power <- audio_object$Power_spectrum
+  appr <- smooth.basis(argvals = freq, y = power ,fdParobj = my_basis)$fd
+  appr <- eval.fd(0:(audio_object$Sampling_rate/2),appr)
+  appr <- appr / sum(appr)
+  
+  return(list("PS_smooth_func" = appr,
+              "Label" = audio_object$Label))
+}
+
+# Get smooth approximations from a list of audio objects 
+get_smooth_list_from_ps <- function(basis_func, n_basis, list){
+  
+  n_iter <- length(list)
+  
+  # setup progress bar
+  pb <- txtProgressBar(min = 0,      # Minimum value of the progress bar
+                       max = n_iter, # Maximum value of the progress bar
+                       style = 3,    # Progress bar style (also available style = 1 and style = 2)
+                       width = 50,   # Progress bar width. Defaults to getOption("width")
+                       char = "=")   # Character used to create the bar
+  i = 1 
+  
+  result <- list()
+  for(audio_object in list){
+    result <- append(result, list(get_smooth_from_ps(basis_func, n_basis, audio_object)))
+    setTxtProgressBar(pb, i)
+    i = i + 1 
+  }
+  close(pb) 
+  return(result)
+}
+
+
+# KNN prediction ----------------------------------------------------------
+
+# Predict a single audio 
+KNN_predict_audio <- function(train_set, new_data, k){
+  
+  weight_label_dataframe <- data.frame()
+  
+  for(train_audio in train_set){
+    weight <-  Kernel( L2(train_audio$PS_smooth_func,new_data$PS_smooth_func) ) 
+    weight_label_dataframe <- rbind(weight_label_dataframe,c(weight,train_audio$Label))
+  }
+  
+  names(weight_label_dataframe) <- c("Weight","Label")
+  weight_label_dataframe <- weight_label_dataframe[order(weight_label_dataframe$Weight,decreasing = T),]
+  
+  weight_label_dataframe <- weight_label_dataframe[1:k,]
+  
+  tot_weight <- sum(weight_label_dataframe$Weight)
+  
+  prediction <- sum(weight_label_dataframe$Weight * weight_label_dataframe$Label) / tot_weight
+  
+  return(prediction)
+}
+
+# Predict a set of audios
+KNN_predict_set <- function(train_set,test_set,k){
+  preds <- data.frame()
+  for(audio in test_set){
+    preds <- rbind(preds,
+                   c(audio$Label,KNN_predict_audio(train_set, audio, k)))
+  }
+  names(preds) <- c("Label", "Prediction")
+  preds <- preds[order(preds$Label),]
+}
+
+# All NN prediction  ------------------------------------------------------
+
+# Predict a single audio 
+All_NN_predict_audio <- function(train_set, new_data){
+  
+  weight_label_dataframe <- data.frame()
+  
+  for(train_audio in train_set){
+    weight <-  Kernel( L2(train_audio$PS_smooth_func,new_data$PS_smooth_func) )
+    weight_label_dataframe <- rbind(weight_label_dataframe,c(weight,train_audio$Label))
+  }
+  
+  names(weight_label_dataframe) <- c("Weight","Label")
+
+  tot_weight <- sum(weight_label_dataframe$Weight)
+  
+  prediction <- sum(weight_label_dataframe$Weight * weight_label_dataframe$Label) / tot_weight
+  
+  return(prediction)
+}
+
+# Predict a set of audios
+All_NN_predict_set <- function(train_set,test_set){
+  preds <- data.frame()
+  for(audio in test_set){
+    preds <- rbind(preds,
+                   c(audio$Label,All_NN_predict_audio(train_set, audio)))
+  }
+  names(preds) <- c("Label", "Prediction")
+  preds <- preds[order(preds$Label),]
 }
 
