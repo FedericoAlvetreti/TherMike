feb7bfe0f0006802abde6d22516d8e935ee04eae
LeoDiNino97 <93933755+LeoDiNino97@users.noreply.github.com>
2023-09-21 11:55:22 +0200

    Delete EDA.R

--- EDA.R
+++
@@ -1,8 +0,0 @@
-# Library -----------------------------------------------------------------
-source('utils.R')
-
-# Loading data ------------------------------------------------------------
-my_recordings <- get_audio_objects("SL-splash-recordings")
-
-# An example
-pw_plot(my_recordings[[84]])
